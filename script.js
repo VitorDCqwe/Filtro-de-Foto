214db956d986b6bbecfc61ae5a6990621b185e2e
Vitor Duarte Chaves <vitord.chaves@gmail.com>
2025-09-29 14:29:04 -0300

    Atualização JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let noseX = 0;
- let noseY = 0;
+let noseY = 0;
 let mustache;
 let canvas;
 let video;
@@ -7,4 +7,41 @@ let poseNet;
 
 function preload() {
     mustache = loadImage("mustache.png");
+}
+
+function setup() {
+    canvas = createCanvas(800,600);
+    canvas.parent("canvas-container");
+    
+    video = createCapture(VIDEO);
+    video.size(800,600)
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses)
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized!")
+}
+
+function gotPoses(results) {
+    if (results.length > 0) 
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+    }
+}
+
+function draw() {
+    background(0);
+    image(video, 0, 0, width, height);
+    image(catNose, noseX - 200, noseY - 140, 400, 400);
+}
+
+function takeSnapshot() {
+    save('myFilterImage.png');
 }
